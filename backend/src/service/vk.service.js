6d38260009307a122979bbb9a2ee1831a3c5077c
Sergey Antonov <flex62ryz@ya.ru>
2019-07-10 23:19:48 +0300

    vk message

--- backend/src/service/vk.service.js
+++ backend/src/service/vk.service.js
@@ -54,7 +54,7 @@ const sendVk = async ({message, usersVk}) => {
 
 const notifyGoodBecameCheaper = async ({id, url, price, old_price, title, usersVk, prev_price, shop_id}) => {
   const oldPriceText = !!old_price ? `${old_price}р ->` : ``;
-  const priceDiff = old_price - price;
+  const priceDiff = old_price && old_price - price;
   const priceDiffText = !!priceDiff ? `-${priceDiff}р` : ``;
   const lastPriceDiff = prev_price - price;
   const lastPriceDiffText = lastPriceDiff !== 0 ? `Динамика цены -${lastPriceDiff}р` : `Появился в наличии`;
