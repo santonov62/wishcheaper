99e6ab978a4c59867d29e7c5ef2c61fcf20abfa8
santonov62 <flex62ryz@ya.ru>
2019-07-22 15:34:11 +0300

    Ui optimization

--- frontend/src/page/MainPage.jsx
+++ frontend/src/page/MainPage.jsx
@@ -5,6 +5,8 @@ import './mainPage.css';
 import {connect} from 'react-redux';
 import SignInWithVkButton from '../component/SignInWithVkButton';
 import {authWithVk, signOut} from "../actionCreators/user.actionCreators";
+import { Redirect } from 'react-router-dom';
+
 
 class MainPage extends React.Component{
   constructor(props) {
@@ -24,33 +26,40 @@ class MainPage extends React.Component{
     const {user} = this.props;
     const {goods_count, week_goods_count, users_count} = this.state;
     return (
-      <div className='mainPage'>
-        <div className='statistic'>
-
-          <div className='title'>Покупайте любимые товары дешевле</div>
-          <div className='stats'>
-            <div className='number'>
-              <div className='decorHeader'>{goods_count}</div>
-              Отслеживаемых товаров
-            </div>
-            <div className='number'>
-              <div className='decorHeader'>{week_goods_count}</div>
-              Новых за неделю
-            </div>
-            <div className='number'>
-              <div className='decorHeader'>{users_count}</div>
-              Пользователей
+        <Fragment>
+          {!!user && user.id &&
+          <Redirect to={{pathname: '/mylist'}}/>
+          }
+          {!user || !user.id &&
+          <div className='mainPage'>
+            <div className='statistic'>
+  
+              {/*{!user.id &&*/}
+              <Fragment>
+                <br/>
+                <br/>
+                <SignInWithVkButton size='massive' text='Авторизироваться'/>
+              </Fragment>
+              {/*}*/}
+              <div className='title'>Покупайте любимые товары дешевле</div>
+              <div className='stats'>
+                <div className='number'>
+                  <div className='decorHeader'>{goods_count}</div>
+                  Отслеживаемых товаров
+                </div>
+                <div className='number'>
+                  <div className='decorHeader'>{week_goods_count}</div>
+                  Новых за неделю
+                </div>
+                <div className='number'>
+                  <div className='decorHeader'>{users_count}</div>
+                  Пользователей
+                </div>
+              </div>
             </div>
           </div>
-          {!user.id &&
-          <Fragment>
-            <br/>
-            <br/>
-            <SignInWithVkButton size='massive' text='Авторизироваться'/>
-          </Fragment>
           }
-        </div>
-      </div>
+        </Fragment>
     );
   }
 }
