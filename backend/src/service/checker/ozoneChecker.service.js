c0293e6ebf01303b3399ba7fa6f86075dd4fac4b
Sergey Antonov <flex62ryz@ya.ru>
2019-10-05 02:21:47 +0300

    Ozone checker, ui

--- backend/src/service/checker/ozoneChecker.service.js
+++ backend/src/service/checker/ozoneChecker.service.js
@@ -55,12 +55,13 @@ const parse = async (url) => {
     
     log(`price`);
     try {
-      currentPrice = await page.$eval('[data-test-id=saleblock-first-price]', node => parseInt(node.innerText.replace(/\s/g, '')));
+      currentPrice = await page.$eval('.top-sale-block>div>div:nth-child(1)>div>div>div>div span:nth-of-type(1)', node => parseInt(node.innerText.replace(/\s/g, '')));
     } catch (e) { }
   
     log(`oldPrice`);
     try {
-      oldPrice = await page.$eval('.top-sale-block div div div div div', node => parseInt(node.innerText.replace(/\s/g, '')));
+      // oldPrice = await page.$eval('.top-sale-block div div div div div', node => parseInt(node.innerText.replace(/\s/g, '')));
+      oldPrice = await page.$eval('.top-sale-block>div>div:nth-child(1)>div>div>div>div span:nth-of-type(2)', node => parseInt(node.innerText.replace(/\s/g, '')));
     } catch (e) { }
     
     log(`logo`);
