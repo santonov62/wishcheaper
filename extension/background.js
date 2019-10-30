effb736f47d49d0cc94f610968118cfb383f24e3
santonov62 <flex62ryz@ya.ru>
2019-10-30 13:59:43 +0300

    chrome extension refactoring

--- extension/background.js
+++ extension/background.js
@@ -3,165 +3,40 @@ chrome.runtime.onStartup.addListener(function() {
 
 });
 
-// const DOMAIN = `https://wishcheaper.herokuapp.com`;
-const DOMAIN = `http://localhost:3000`;
-
-function getUrlParameterValue(url, parameterName) {
-  "use strict";
-  
-  var urlParameters  = url.substr(url.indexOf("#") + 1),
-      parameterValue = "",
-      index,
-      temp;
-  
-  urlParameters = urlParameters.split("&");
-  
-  for (index = 0; index < urlParameters.length; index += 1) {
-    temp = urlParameters[index].split("=");
-    
-    if (temp[0] === parameterName) {
-      return temp[1];
-    }
-  }
-  
-  return parameterValue;
-}
-
-function listenerHandler(authenticationTabId, resolve, reject) {
-  
-  return function tabUpdateListener(tabId, changeInfo) {
-    var vkAccessToken,
-        vkAccessTokenExpiredFlag;
-    
-    if (tabId === authenticationTabId && changeInfo.url !== undefined && changeInfo.status === "loading") {
-      
-      if (changeInfo.url.indexOf('oauth.vk.com/blank.html') > -1) {
-        authenticationTabId = null;
-        chrome.tabs.onUpdated.removeListener(tabUpdateListener);
-        
-        vkAccessToken = getUrlParameterValue(changeInfo.url, 'access_token');
-        
-        if (vkAccessToken === undefined || vkAccessToken.length === undefined) {
-          displayeAnError('vk auth response problem', 'access_token length = 0 or vkAccessToken == undefined');
-          return;
-        }
-        
-        vkAccessTokenExpiredFlag = Number(getUrlParameterValue(changeInfo.url, 'expires_in'));
-        
-        if (vkAccessTokenExpiredFlag < 0) {
-          reject('expired');
-          displayeAnError('vk auth response problem', 'vkAccessTokenExpiredFlag != 0' + vkAccessToken);
-          return;
-        }
-        
-        chrome.storage.local.set({'vkaccess_token': vkAccessToken}, () => {
-          chrome.tabs.remove([tabId], () => {
-            resolve(vkAccessToken);
-            alert(`Vk token: ${vkAccessToken}`);
-          });
+function vkAuthListener(ownerTabId) {
+  return function checkAuthSuccess(tabId, changeInfo, tab) {
+    if (tabId === ownerTabId) {
+      console.log(`waiting authData...`);
+      if (tab.status === 'complete' && tab.url.indexOf('login') === -1) {
+        chrome.tabs.onUpdated.removeListener(checkAuthSuccess);
+        chrome.tabs.executeScript(tabId, {
+          file: "getAuthData.js"
+        }, () => {
+          console.log('done');
+          console.groupEnd();
+          chrome.tabs.remove(tabId, () => {});
         });
       }
     }
   }
 }
 
-async function addUrl(url) {
-  try {
-    const authData = await authVk();
-    if (!authData)
-      throw new Error(`authData required`);
-    
-    const good = await fetch(`${DOMAIN}/checker/add`, {
-      method: 'POST',
-      body: JSON.stringify({
-        url
-      }),
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${authData.token}`
-      }
-    }).then(res => res.json());
-    if (good.error)
-      throw new Error(good.error);
-    console.log("[addUrl] good", good);
-    alert('Product added');
-    return good;
-  } catch (e) {
-    alert(e.message);
-  }
-}
-
-function authVk() {
-  return new Promise((resolve, reject) => {
-    try {
-      chrome.storage.local.get(['authData'], async ({authData}) => {
-        
-        if (!authData) {
-          const token = await getVkAccessToken();
-          const authData = await fetch(`${DOMAIN}/auth/vk/token`, {
-          // const authData = await fetch('http://localhost:3000/auth/vk/token', {
-            method: 'POST',
-            body: JSON.stringify({token}),
-            headers: {
-              'Content-Type': 'application/json'
-            }
-          }).then(response => response.json());
-      
-          chrome.storage.local.set({authData}, () => {
-            resolve(authData);
-          });
-          
-        } else {
-          resolve(authData)
-        }
-      });
-      
-    } catch (e) {
-      reject(e.message);
-    }
-  })
-  
-}
-
-function getVkAccessToken() {
-  
-  return new Promise((resolve, reject) => {
-    
-    // chrome.storage.local.get({'vkaccess_token': {}}, ({vkaccess_token}) => {
-  
-      // if (vkaccess_token.length === undefined) {
-        // const vkCLientId = '7173995';
-        // const vkCLientId = '7037811';
-        const vkCLientId = '7039043';
-        const vkAuthenticationUrl = 'https://oauth.vk.com/authorize?client_id=' + vkCLientId + '&redirect_uri=http%3A%2F%2Foauth.vk.com%2Fblank.html&display=page&response_type=token';
-  
-        chrome.tabs.create({url: vkAuthenticationUrl, selected: true}, (tab) => {
-          const authenticationTabId = tab.id;
-          chrome.tabs.onUpdated.addListener(listenerHandler(authenticationTabId, resolve, reject));
-        });
-        
-      // } else {
-      //   resolve(vkaccess_token);
-      // }
-      
-    // });
-  });
-  
-}
-
-function displayeAnError(textToShow, errorToShow) {
-  "use strict";
-  
-  alert(textToShow + '\n' + errorToShow);
-}
-
-chrome.contextMenus.create({
-  id: "add",
-  title: "Bookmark to wishcheaper"
-});
-
-chrome.contextMenus.onClicked.addListener(function(info, tab) {
-  if (info.menuItemId === "add") {
-    addUrl(info.pageUrl);
+chrome.runtime.onMessage.addListener( ({action, data}) => {
+  console.log(`action: ${action}`, data);
+  if (action === 'vkAuth') {
+    const url = data.url;
+    console.group(`[vkAuth]`);
+    console.log('chrome.tabs.create ', url);
+    chrome.tabs.create({url, selected: true}, (tab) => {
+      console.log('done');
+      chrome.tabs.onUpdated.addListener(vkAuthListener(tab.id));
+    });
+  } else if (action === 'authData') {
+    console.group(`[authData]`);
+    console.log('chrome.storage.local.set', data);
+    chrome.storage.local.set({authData: data}, () => {
+      console.log('done');
+      console.groupEnd();
+    });
   }
 });
--- extension/popup.js
+++ extension/popup.js
@@ -1,15 +1,17 @@
+const DOMAIN = `https://wishcheaper.herokuapp.com`;
+
 window.onload = () => {
-  // injectVkScript();
 
   const vkButton = document.getElementById('vk');
   vkButton.onclick = vkAuth;
 
-  const addByUrlButton = document.getElementById('addByUrl');
-  addByUrlButton.onclick = addByUrl;
+  const addButton = document.getElementById('add');
+  addButton.onclick = addCurrent;
 
   chrome.storage.local.get(['authData'], ({authData}) => {
     if (!!authData) {
-      addByUrlButton.classList.remove('hidden');
+      // addButton.classList.remove('hidden');
+      addCurrent();
     } else {
       vkButton.classList.remove('hidden');
     }
@@ -18,41 +20,107 @@ window.onload = () => {
 
 };
 
-function addByUrl() {
-
+function addCurrent() {
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, (tabs) => {
+    const tab = tabs[0];
+    console.log(tab)
+      addByUrl(tab.url)
+  })
 }
 
 function vkAuth() {
-
+  chrome.runtime.sendMessage('', {action: 'vkAuth', data: { url: `${DOMAIN}/login` }});
 }
 
-// const injectVkScript = () => {
-//     let script = document.createElement('script');
-//
-//     script.src = "openapi.js";
-//     document.head.append(script);
-//
-//     script.onload = () => {
-//         window.VK.init({
-//             apiId: 7037811
-//         });
-//         hideSpinner();
-//     };
-//     script.onerror = () => {
-//         log('vkApiError');
-//     };
-// };
+async function addByUrl(url) {
+  console.group('[addByUrl] ', url);
+  showSpinner();
+  try {
+    let authData = await getAuthData();
+    if (!authData)
+      throw new Error(`authData required`);
+
+    authData = JSON.parse(authData);
+
+    const good = await fetch(`${DOMAIN}/checker/add`, {
+      method: 'POST',
+      body: JSON.stringify({
+        url
+      }),
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${authData.user.token}`
+      }
+    }).then(res => res.json());
+    if (good.error)
+      throw new Error(good.error);
+    console.log("[addUrl] good", good);
+    showMessage(`Товар добавлен`);
+    return good;
+  } catch (e) {
+    showMessage(e.message);
+  } finally {
+    console.groupEnd();
+    hideSpinner();
+  }
+}
+
+async function getAuthData() {
+  return new Promise((resolve, reject) => {
+    chrome.storage.local.get(['authData'], async ({authData}) => {
+      if (!authData) {
+        reject(authData)
+      } else {
+        resolve(authData)
+      }
+    });
+  });
+}
+
+const showSpinner = () => {
+  const spinner = document.getElementById(`spinner`);
+  spinner.classList.remove('hidden');
+  hideContent();
+};
+const hideSpinner = () => {
+  const spinner = document.getElementById(`spinner`);
+  spinner.classList.add(`hidden`);
+  showContent();
+};
+
+const showContent = () => {
+  const content = document.getElementById(`content`);
+  content.classList.remove('hidden');
+};
+const hideContent = () => {
+  const content = document.getElementById(`content`);
+  content.classList.add('hidden');
+};
+
+const showMessage = (text) => {
+  hideButtons();
+  const el = document.getElementById(`message`);
+  el.innerText = text;
+  el.classList.remove(`hidden`);
+};
+const hideMessage = () => {
+  const message = document.getElementById(`message`);
+  message.classList.add(`hidden`);
+  showButtons();
+};
+
+const hideButtons = () => {
+  const el = document.getElementById(`buttons`);
+  el.classList.add(`hidden`);
+};
+const showButtons = () => {
+  const el = document.getElementById(`buttons`);
+  el.classList.remove(`hidden`);
+};
 
 const log = (value) => {
   console.log(value);
-};
-
-// const showSpinner = () => {
-//     const spinner = document.querySelector(`.loader`);
-//     spinner.classList.add(`show`);
-// };
-
-const hideSpinner = () => {
-  const spinner = document.querySelector(`.loader`);
-  spinner.classList.add(`hidden`);
 };
