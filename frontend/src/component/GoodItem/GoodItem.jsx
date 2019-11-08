9c88c10729801af4d489479d42b2e5f6c21a2a17
santonov62 <flex62ryz@ya.ru>
2019-11-08 16:38:34 +0300

    Product item ui

--- frontend/src/component/GoodItem/GoodItem.jsx
+++ frontend/src/component/GoodItem/GoodItem.jsx
@@ -80,9 +80,9 @@ const ShortGoodItem = ({id: good_id, url, title, logo, price, old_price, shop_id
         <Card.Content>
           <div style={{color: '#91998c', fontSize: '13px', marginBottom: '3px'}}><Icon name='shop' />{shop_name}</div>
           <Card.Header className='title' as='a' href={url} target='_blank' title={title}>
-            {isImportant &&
-              <Icon title="Выгодная цена" color="red" name='fire'/>
-            }
+            {/*{isImportant &&*/}
+              {/*<Icon title="Выгодная цена" color="red" name='fire'/>*/}
+            {/*}*/}
             {title}
           </Card.Header>
           {/*<Card.Meta>*/}
