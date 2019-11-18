197145714083a60088ef3e19b11c61c72e346adc
Sergey Antonov <flex62ryz@ya.ru>
2019-11-19 00:29:24 +0300

    My shops order

--- backend/src/service/shops.service.js
+++ backend/src/service/shops.service.js
@@ -47,7 +47,7 @@ SELECT sh.id, sh.title, sh.name,
 FROM shops sh
      INNER JOIN goods g ON g."shop_id" = sh.id
      INNER JOIN subscriptions s ON s."good_id" = g.id AND s."user_vk" = $1
-GROUP BY sh.id`;
+GROUP BY sh.title`;
 const myShops = async({vk}) => {
   const result = await db.query(MY_SHOPS, [vk]);
   return result && result.rows;
