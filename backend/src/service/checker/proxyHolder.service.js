c6f5a5a025a4a6010e3476947877a5b85ed7d598
santonov62 <flex62ryz@ya.ru>
2019-11-22 15:12:21 +0300

    proxiesList.length === 0

--- backend/src/service/checker/proxyHolder.service.js
+++ backend/src/service/checker/proxyHolder.service.js
@@ -75,7 +75,7 @@ const unshiftProxy = (proxy) => {
 };
 const isProxiesNeedUpdate = () => {
   const isExpired = Date.now() - lastUpdateTime > 60000 * 10;
-  const isPoor = !proxiesList || proxiesList.length < 10;
+  const isPoor = !proxiesList || proxiesList.length === 0;
   return isExpired || isPoor;
 };
 
