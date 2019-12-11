9f7fedc5f77a1685740a2dee3eedb3cdbf48c7f5
santonov62 <flex62ryz@ya.ru>
2019-12-11 18:14:34 +0300

    Playstation store checker

--- backend/src/service/checker/checker.service.js
+++ backend/src/service/checker/checker.service.js
@@ -21,6 +21,7 @@ const asosChecker = require('./asosChecker.service');
 const leroymerlinChecker = require('./leroymerlinChecker.service');
 const citilinkChecker = require('./citilinkChecker.service');
 const computeruniverseChecker = require('./computeruniverseChecker.service');
+const playstationStoreChecker = require('./playstationStoreChecker.service');
 const moment = require('moment');
 
 const checkerList = [
@@ -41,6 +42,7 @@ const checkerList = [
   leroymerlinChecker,
   wildberriesChecker,
   citilinkChecker,
+  playstationStoreChecker,
   computeruniverseChecker
 ];
 
