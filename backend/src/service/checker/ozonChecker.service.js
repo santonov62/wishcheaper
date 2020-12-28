0ca04355a6a1b4c1088fbfd4b6e9f797d5af8bb5
Sergey_Antonov <flex62ryz@gmail.com>
2020-12-28 14:27:38 +0300

    Ozon checker

---
+++ backend/src/service/checker/ozonChecker.service.js
@@ -0,0 +1,128 @@
+const puppeteer = require('puppeteer');
+const shopService = require('../shops.service');
+const TIMEOUT_DELAY = 30000;
+const SHOP_NAME = 'ozon.ru';
+const SHOP_TITLE = 'Ozone';
+const iPhone = puppeteer.devices['iPhone 6'];
+const SCAN_INTERVAL_MINUTES = 720;
+
+const log = (text, params = '') => {
+  console.log(`[ozonChecker.service] -> ${text}`, params);
+};
+
+const init = async () => {
+  const shop = await shopService.getShopByUrl(SHOP_NAME);
+  if (!shop) {
+    const addedShop = await shopService.save({
+      title: SHOP_TITLE,
+      url: `https://${SHOP_NAME}`,
+      name: SHOP_NAME,
+      scan_interval: SCAN_INTERVAL_MINUTES});
+    log(`[init] added shop`, addedShop);
+  }
+};
+
+init();
+
+const parse = async (url) => {
+  
+  if (!url)
+    throw new Error(`Url required.`);
+
+  let launchParams = { args: [ `--no-sandbox` ], headless: !process.env.PUPPETEER_DEV };
+
+  const browser = await puppeteer.launch(launchParams);
+
+  try {
+    const page = await browser.newPage();
+    await page.emulate(iPhone);
+
+    log(`goto: `, url);
+    await page.goto(url, {waitUntil: 'domcontentloaded', timeout: TIMEOUT_DELAY});
+    
+    let title, currentPrice, logo, oldPrice, inactive_at;
+    log(`title`);
+    try {
+      await page.waitForSelector('[data-widget="webProductHeading"]', { visible: true });
+      title = await page.$eval('[data-widget="webProductHeading"]', node => node.textContent);
+    } catch (e) {
+      console.error(e);
+    }
+
+    log(`prices`);
+    let prices;
+    try {
+      await page.waitForSelector('[data-widget="webPrice"]', { visible: true });
+      prices = await page.$eval('[data-widget="webPrice"]', node => {
+        const text = node.innerText.split('\n');
+        console.log(text)
+        const pricesBox = text[text.length - 1];
+        return pricesBox.replace(/\s/g, '').split('₽');
+      });
+    } catch (e) {
+      console.error(e);
+    }
+
+    log(`price`);
+    try {
+      currentPrice = parseInt(prices[0]);
+    } catch (e) {
+      console.error(e);
+    }
+  
+    log(`oldPrice`);
+    try {
+      oldPrice = parseInt(prices[1]);
+    } catch (e) {
+      console.error(e);
+    }
+
+    log(`logo`);
+    try {
+      logo = await page.$eval('[data-widget="webMobGallery"] img', node => node.getAttribute('src'));
+    } catch (e) {
+      console.error(e);
+    }
+
+    try {
+      const innactive = await page.$('[data-widget="webOutOfStock"]');
+      if (!!innactive) {
+        inactive_at = new Date();
+      }
+    } catch (e) {
+      console.error(e);
+    }
+    // if (!currentPrice) {
+    //   inactive_at = new Date();
+    // }
+    
+    const parsedData = {
+        url,
+        title,
+        price: currentPrice,
+        old_price: oldPrice,
+        logo,
+        inactive_at
+    };
+    
+    log(`[parse] done`, parsedData);
+
+    return parsedData;
+
+  } catch (e) {
+    throw new Error(e);
+  } finally {
+    if (!process.env.PUPPETEER_KEEP_OPENED)
+      await browser.close();
+  }
+};
+
+const isMyUrl = (url) => {
+  return url.indexOf(SHOP_NAME) !== -1;
+};
+
+module.exports = {
+  parse,
+  isMyUrl,
+  getShopUrl: () => SHOP_NAME
+};
