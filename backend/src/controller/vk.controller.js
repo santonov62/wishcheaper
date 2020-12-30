2c14b566ec11671047ddc669d401af2c14a8dde5
Sergey_Antonov <flex62ryz@gmail.com>
2020-12-30 13:07:05 +0300

    Vk callback api

--- backend/src/controller/vk.controller.js
+++ backend/src/controller/vk.controller.js
@@ -10,11 +10,30 @@ const callbackApi = async (req, res) => {
   try {
     log(req.body);
     // { "type": "confirmation", "group_id": 183983399 }
-    const { type, group_id } = req.body;
+    // {
+    //   "type": "message_new",
+    //     "object": {
+    //   "id": 8318,
+    //       "date": 1609326083,
+    //       "out": 0,
+    //       "user_id": 2758589,
+    //       "read_state": 0,
+    //       "title": "",
+    //       "body": "324234234",
+    //       "owner_ids": []
+    // },
+    //   "group_id": 183983399,
+    //     "event_id": "03eeb977e94bd1604533b79a77751db6b8798527",
+    //     "secret": "mNrFM3EPU9nmqA4g8UQs"
+    // }
+    const { type, group_id, secret, object: { body, user_id } } = req.body;
     if (type === 'confirmation') {
-        res.json('b8660ca3');
+        res.send('b8660ca3');
+        return;
+    } else if (type === "message_new") {
+
     }
-    res.json('ok');
+    res.send('ok');
   } catch (e) {
     res.status(500).json({error: e.message});
   } finally {
