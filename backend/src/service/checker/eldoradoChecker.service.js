3e890d68cf57e19173130613ffeff6f8d68ec5b2
Sergey_Antonov <flex62ryz@gmail.com>
2021-01-14 20:46:47 +0300

    Eldorado checker

--- backend/src/service/checker/eldoradoChecker.service.js
+++ backend/src/service/checker/eldoradoChecker.service.js
@@ -4,6 +4,7 @@ const TIMEOUT_DELAY = 30000;
 const SHOP_NAME = 'eldorado.ru';
 const SHOP_TITLE = 'Эльдорадо';
 const proxyHolder = require('../../module/proxyHolder');
+const iPhone = puppeteer.devices['iPhone 6'];
 
 const log = (text, params = '') => {
   console.log(`[eldoradoChecker.service] -> ${text}`, params);
@@ -28,47 +29,48 @@ const parse = async (url) => {
   if (!url)
     throw new Error(`Url required.`);
 
-  let launchParams = { args: [ `--no-sandbox` ], headless: true };
-  const proxy = await proxyHolder.pullProxy(url);
-  if (!!proxy && !!proxy.ip) {
-    launchParams = {args: [`--proxy-server=${proxy.ip}`, `--no-sandbox`]};
-  }
+  let launchParams = { args: [ `--no-sandbox` ] };
+  // const proxy = await proxyHolder.pullProxy(url);
+  // if (!!proxy && !!proxy.ip) {
+  //   launchParams = {args: [`--proxy-server=${proxy.ip}`, `--no-sandbox`]};
+  // }
 
   const browser = await puppeteer.launch({ ...launchParams, headless: !process.env.PUPPETEER_DEV });
 
   try {
     const page = await browser.newPage();
+    await page.emulate(iPhone);
 
     log(`goto: `, url);
     await page.goto(url, {waitUntil: 'domcontentloaded', timeout: TIMEOUT_DELAY});
     // log(`done`);
 
-    let inactive_at;
-    const payButton = await page.$('.priceContainer .addToCartBigRP');
-    if (!payButton) {
-      inactive_at = new Date();
-    }
-
     let title, currentPrice, logo, oldPrice;
     log(`title`);
     try {
-      title = await page.$eval('.catalogItemDetailHd', node => node.innerText);
+      title = await page.$eval('.page_name__title', node => node.innerText);
     } catch (e) { }
 
     log(`price`);
     try {
-      currentPrice = await page.$eval('.priceContainer .product-box-price__active', node => parseInt(node.innerText.replace(/[^0-9,]/g, '')));
+      currentPrice = await page.$eval('.container .new-price', node => parseInt(node.innerText.replace(/[^0-9,]/g, '')));
     } catch (e) { }
 
     log(`oldPrice`);
     try {
-      oldPrice = await page.$eval('.product-box-price__old-el', node => parseInt(node.innerText.replace(/[^0-9,]/g, '')));
+      oldPrice = await page.$eval('.container .old-price', node => parseInt(node.innerText.replace(/[^0-9,]/g, '')));
     } catch (e) { }
 
     log(`logo`);
     try {
-      logo = await page.$eval('.slider-with-preview .slider-item-active img', node => node.getAttribute('src'))
+      logo = await page.$eval('.container .preview-item-body__img', node => node.getAttribute('src'))
     } catch (e) { }
+
+    let inactive_at;
+    const payButton = await page.$('.preview-add .js-add-to-basket');
+    if (!payButton) {
+      inactive_at = new Date();
+    }
 
     const parsedData = {
       url,
@@ -86,7 +88,8 @@ const parse = async (url) => {
   } catch (e) {
     throw new Error(e);
   } finally {
-    await browser.close();
+    if (!process.env.PUPPETEER_KEEP_OPENED)
+      await browser.close();
   }
 };
 
