18e8fcd72152fe346e408702adbb625445dd828d
flex62ryz <flex62ryz@ya.ru>
2026-04-10 19:52:46 +0300

    chore: upgrade pg dependency, enable node inspector in dev, simplify db connection, and update VK environment variables

--- backend/src/service/db.service.js
+++ backend/src/service/db.service.js
@@ -1,8 +1,5 @@
 const { Pool } = require('pg');
 const connectionString = require('../../knexfile')[process.env.NODE_ENV].connection;
 const pool = new Pool({ connectionString: connectionString });
-pool.connect(err => {
-  if (err) throw err;
-});
 
 module.exports = pool;
